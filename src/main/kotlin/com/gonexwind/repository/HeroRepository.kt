a9bb445ec033f2fd39f2d383b1dacbba2b29f5fd
gonexwind <fikky.ardi15@gmail.com>
2022-09-05 17:12:07 +0700

    add repository

---
+++ src/main/kotlin/com/gonexwind/repository/HeroRepository.kt
@@ -0,0 +1,20 @@
+package com.gonexwind.repository
+
+import com.gonexwind.models.ApiResponse
+import com.gonexwind.models.Hero
+
+interface HeroRepository {
+
+    val heroes: Map<Int, List<Hero>>
+
+    val page1: List<Hero>
+    val page2: List<Hero>
+    val page3: List<Hero>
+    val page4: List<Hero>
+    val page5: List<Hero>
+
+    suspend fun getAllHeroes(page: Int = 1): ApiResponse
+
+    suspend fun searchHeroes(name: String): ApiResponse
+
+}
